64c6b0deceac0f6bc276af9ee4f97ad0d8847271
lizhong <13590316596@163.com>
2023-04-09 14:16:25 +0800

    创建各个中间件子工程

---
+++ mogodb/src/test/java/com/qin/mogodb/MogodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qin.mogodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MogodbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ redis/springboot整合redis笔记.md
@@ -0,0 +1,12 @@
+
+
+> pom.xml加入依赖
+```xml
+<dependency>
+    <groupId>org.springframework.boot</groupId>
+    <artifactId>spring-boot-starter-data-redis</artifactId>
+</dependency>
+```
+
+
+> 在application.properties中加入redis配置
