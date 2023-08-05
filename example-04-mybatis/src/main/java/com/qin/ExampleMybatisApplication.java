a6899e3c07e1d9f48642ce5342812fa0a31d94b5
lizhong <13590316596@163.com>
2023-08-06 06:55:23 +0800

    调整module名称 --- 2023/08/6

--- example-04-mybatis/src/main/java/com/qin/ExampleMybatisApplication.java
+++ example-04-mybatis/src/main/java/com/qin/ExampleMybatisApplication.java
@@ -3,15 +3,14 @@ package com.qin;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 
 @SpringBootApplication()
 @MapperScan("com.qin.mapper")
-public class MybatisApplication {
+public class ExampleMybatisApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MybatisApplication.class, args);
+        SpringApplication.run(ExampleMybatisApplication.class, args);
         System.out.println("------------------  mybatis project running  ------------------");
     }
 }
