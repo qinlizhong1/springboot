049fb4ddae7dd662b24e6906b545b164f79e4041
lizhong <13590316596@163.com>
2023-09-26 05:27:22 +0800

    springboot hdfs整合 --- 2023/09/21

---
+++ example-37-hdfs/src/main/java/com/qin/Example37HdfsApplication.java
@@ -0,0 +1,13 @@
+package com.qin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Example37HdfsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Example37HdfsApplication.class, args);
+    }
+
+}
